ad58c203c22ef6e4761367a2039ffcbed1353e35
Aaron Xiang <aaron.xiang@gmail.com>
2023-03-07 16:58:49 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+const alanKey = 'c1014e7b0b16c7ed556b1ce11a5908d32e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === 'testCommand') {
+          alert('This code was executed')
+        }
+      }
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Alan AI New Application</div>
+  )
 }
 
 export default App;
